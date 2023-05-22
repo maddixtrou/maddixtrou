37cc3ea489f840e7b9765e3978259a3c456ebc57
maddixtrou <troull@stcharles.k12.la.us>
2023-05-22 05:15:25 +0000

    runtime

--- projects/runtime/js/runLevels.js
+++ projects/runtime/js/runLevels.js
@@ -13,12 +13,69 @@ var runLevels = function (window) {
     // behavior of our game
     var levelData = window.opspark.levelData;
 
+    window.levelData = levelData;
+
+    for (var i = 0; i < levelData.gameItems.length; i++) {
+      var eachItem = levelData.gameItems[i];
+      if (eachItem.type === "sawBlade") {
+        createSawBlade(550, 350);
+        createSawBlade(340, 500);
+        createSawBlade(400, 600);
+        createSawBlade(380, 800);
+      }
+      if (eachItem.type === "brokenHeart"){
+        brokenHeart(400, 300);
+        brokenHeart(600, 500);
+      }
+    if (eachItem.type === "enemy"){
+      createEnemy(900, groundY - 400);
+      createEnemy(1200, groundY - 500);
+      createEnemy(1600, groundY - 600);
+    }
+    if (eachItem.type === "reward"){
+      createReward(800, groundY - 450);
+      createReward(1500, groundY - 550);
+    }
+  };
+
+  var levelData = {
+    name: "Henry",
+    number: 1,
+    speed: -3
+    gameItems: [
+      { type: "sawBlade", x:550, y:350 },
+      { type: "sawBlade", x:340, y:500 },
+      { type: "sawBlade", x:400, y:600 },
+      { type: "sawBlade", x:300, y:800 },
+      { type: "brokenHeart", x:400, y:300 },
+      { type: "brokenHeart", x:600, y:500 },
+      { type: "enemy", x:900, y:- 400 },
+      { type: "enemy", x:1200, y:- 500 },
+      { type: "enemy", x:1600, y:- 600 },
+      { type: "reward", x:800, y:- 450}
+      { type: "reward", x:1500, y:- 550},
+    ],
+  };
+
     // set this to true or false depending on if you want to see hitzones
     game.setDebugMode(true);
 
     // TODOs 5 through 11 go here
     // BEGIN EDITING YOUR CODE HERE
+    function createSawBlade(x, y) {
+      var hitZoneSize = 25;
+      var damageFromObstacle = 10;
+      var sawBladeHitZone = game.createObstacle(hitZoneSize, damageFromObstacle);
 
+      sawBladeHitZone.x = x;
+      sawBladeHitZone.y = y;
+      game.addGameItem(sawBladeHitZone);
+
+      var obstacleImage = draw.bitmap("img/sawblade.png");
+      sawBladeHitZone.addChild(obstacleImage);
+      obstacleImage.x = -25;
+      obstacleImage.y = -25;
+    }
 
 
     function createSawBlade(x, y) {
